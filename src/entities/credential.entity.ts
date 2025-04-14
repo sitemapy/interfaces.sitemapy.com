1d5dc0208e6ca42b7f200423a21d0d6447fe8ace
Kevin Marques <marques.developpement@gmail.com>
2025-04-14 11:00:34 +0200

    feat: add credential entity

---
+++ src/entities/credential.entity.ts
@@ -0,0 +1,6 @@
+export type CredentialEntity = {
+  pfk_user_id: string;
+  google_access_token: string;
+  google_refresh_token: string;
+  google_expiration_date: Date;
+};
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,3 +2,4 @@ export * from "./error.entity";
 export * from "./user.entity";
 export * from "./organization.entity";
 export * from "./sitemap.entity";
+export * from "./credential.entity";
