55376458c063367cd69a85069aab74ec6f91d0a7
Kevin Marques <marques.developpement@gmail.com>
2025-04-18 18:35:35 +0200

    feat: add POST /sitemap/crawl/for-free-landing-page

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -3,4 +3,8 @@ import { SitemapPageEntity, SitemapResponse } from "../entities/sitemap.entity";
 export type ApiResponses = {
   ["POST /sitemap/crawl/with-tree"]: SitemapResponse;
   ["POST /sitemap/crawl/only-pages"]: SitemapPageEntity[];
+  ["POST /sitemap/crawl/for-free-landing-page"]: {
+    rate_limit_remaining: number;
+    sitemap: SitemapResponse;
+  };
 };
