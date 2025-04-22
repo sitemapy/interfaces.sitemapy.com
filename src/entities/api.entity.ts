c8e1df76d46f9eb29dcbde4db0977f3568457d09
Kevin Marques <marques.developpement@gmail.com>
2025-04-22 16:20:55 +0200

    feat: extend mode property in ApiLogEntity to include 'sitemap_tree_with_pages'

--- src/entities/api.entity.ts
+++ src/entities/api.entity.ts
@@ -10,7 +10,7 @@ export type ApiLogEntity = {
   total_pages_in_sitemaps: number;
   fetching_duration: number;
   does_sitemap_contain_errors: boolean;
-  mode: "sitemap_tree" | "pages_only";
+  mode: "sitemap_tree" | "pages_only" | "sitemap_tree_with_pages";
 
   created_at: Date;
 };
