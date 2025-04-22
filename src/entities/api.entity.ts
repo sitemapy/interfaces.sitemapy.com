356c512bb5f8a40288a3525371ff2cdd590359ca
Kevin Marques <marques.developpement@gmail.com>
2025-04-22 11:35:30 +0200

    fix: rename api log property

--- src/entities/api.entity.ts
+++ src/entities/api.entity.ts
@@ -7,8 +7,8 @@ export type ApiLogEntity = {
   number_of_sitemap_fetched: number;
   total_pages_in_sitemaps: number;
   fetching_duration: number;
-  status: "success" | "error";
-  error_message: string | null;
+  does_sitemap_contain_errors: boolean;
+
   created_at: Date;
 };
 
