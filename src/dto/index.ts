5abd8fe00a1be78ba6f4e1785286d7259c817eae
Kevin Marques <marques.developpement@gmail.com>
2025-04-26 15:25:04 +0200

    feat: add auth routes

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,10 +1,29 @@
 import { SitemapPageEntity, SitemapResponse } from "../entities/sitemap.entity";
-
+import { UserEntity } from "../entities/user.entity";
 export type ApiResponses = {
   ["POST /sitemap/crawl/with-tree"]: SitemapResponse;
   ["POST /sitemap/crawl/only-pages"]: SitemapPageEntity[];
   ["POST /sitemap/crawl/for-free-landing-page"]: {
     rate_limit_remaining: number;
     sitemap: SitemapResponse;
+  };
+
+  ["POST /auth/login"]: {
+    user: UserEntity;
+    access_token: string;
+  };
+  ["POST /auth/signup"]: {
+    user: UserEntity;
+    access_token: string;
+  };
+  ["GET /auth/me"]: {
+    user: UserEntity;
+  };
+  ["GET /auth/google/url"]: {
+    url: string;
+  };
+  ["GET /auth/google/callback"]: {
+    user: UserEntity;
+    access_token: string;
   };
 };
